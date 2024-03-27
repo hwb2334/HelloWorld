c6d6c196d962e417e3023c633b3fe636b25c4999
ubantu-fixbug <741009304@qq.com>
2024-03-27 14:19:22 +0800

    ushm 优化func排序

--- main.cpp
+++ main.cpp
@@ -1,12 +1,17 @@
 #include<iostream>
 using namespace std;
+// ushm
 void func(int *arr, int l){
 	for(int i = 0;i<l-1;++i){
+		bool flag = false;
 		for(int j=0;j<size-1-i;++j){
 			tmp = arr[i];
 			arr[i] = arr[j];
 			arr[j] = arr[i];
+			flag = true;
 		}
+		if(!flag)
+			break;
 	}
 }
 int main(){
