e403df6058d4129ed7d04e7eeb8d3f62b9d6bfbf
shm <741009304@qq.com>
2024-03-27 14:16:22 +0800

    shm 修改func函数，使数组从小到大排序

--- main.cpp
+++ main.cpp
@@ -3,9 +3,12 @@ using namespace std;
 void func(int *arr, int l){
 	for(int i = 0;i<l-1;++i){
 		for(int j=0;j<size-1-i;++j){
-			tmp = arr[i];
-			arr[i] = arr[j];
-			arr[j] = arr[i];
+			// shm 数组从小到大排序
+			if(arr[j]>arr[j+1]){
+				tmp = arr[i];
+                        	arr[i] = arr[j];
+                        	arr[j] = arr[i];
+			}
 		}
 	}
 }
@@ -13,6 +16,7 @@ int main(){
 	int arr[] = {78,57,23,1};
 	int size = sizeof(arr)/sizeof(arr[0]);
 	func(arr,size);
+	// shm 给注释
 	for(int i=0;i<size;i++){
 		cout<<arr[i]<<" ";
 	}
