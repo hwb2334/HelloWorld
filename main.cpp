6f0081fb7ad6e3137bd24625e8288ab510df840c
ubantu-fixbug <741009304@qq.com>
2024-03-27 12:26:48 +0800

    完成了冒泡排序

--- main.cpp
+++ main.cpp
@@ -1,7 +1,13 @@
 #include<iostream>
 using namespace std;
 void func(int *arr, int l){
-
+	for(int i = 0;i<l-1;++i){
+		for(int j=0;j<size-1-i;++j){
+			tmp = arr[i];
+			arr[i] = arr[j];
+			arr[j] = arr[i];
+		}
+	}
 }
 int main(){
 	int arr[] = {78,57,23,1};
